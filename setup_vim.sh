642d212cd3b7eaf8aaeb8a1ab89c317d9ce23200
Chen, Yu <chrischenubc@gmail.com>
2022-02-20 19:33:39 -0800

    add setup_vim.sh

---
+++ setup_vim.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+VIM_RUNTIME=~/.vim_runtime
+DOTFILES=$(pwd)
+
+if [ ! -d "$VIM_RUNTIME" ] ; then
+   echo "amix vimrc not found, clone the repo"
+   git clone --depth=1 https://github.com/amix/vimrc.git $VIM_RUNTIME
+   sh $VIM_RUNTIME/install_awesome_vimrc.sh
+else
+    echo amix vimrx exists, pull the latest commit""
+    cd "$VIM_RUNTIME"
+    git pull
+   sh $VIM_RUNTIME/install_awesome_vimrc.sh
+fi
+
+cd $DOTFILES
+stow vim
+
+echo "setup successfully"
